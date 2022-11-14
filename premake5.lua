173e74452d8c99a7031b33240ce26cdc3a9323c9
Leonardo Zielinski <lzielinski03@gmail.com>
2022-11-14 00:06:41 -0300

    premake

---
+++ premake5.lua
@@ -0,0 +1,104 @@
+workspace "vulkan-playground"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "vulkan-playground"
+	location "vulkan-playground"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}") 
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}") 
+
+	files
+	{
+		"%{prj.name}/src/**.hpp",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"vulkan-playground/vendor/spdlog/include",
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"VPL_PLATFORM_WINDOWS",
+			"VPL_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/sandobx")
+		}
+
+	filter "configurations:Debug"
+		defines "VPL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "VPL_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "VPL_DIST"
+		optimize "On"
+
+project "sandobx"
+	location "sandobx"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}") 
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}") 
+
+	files
+	{
+		"%{prj.name}/src/**.hpp",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"vulkan-playground/vendor/spdlog/include",
+		"vulkan-playground/src"
+	}
+
+	links {
+		"vulkan-playground"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"VPL_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "VPL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "VPL_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "VPL_DIST"
+		optimize "On"
